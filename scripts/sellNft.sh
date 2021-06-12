528475fef416906fe20792746f74d22d56383937
jesulonimi21 <anjola4jeez@gmail.com>
2021-06-12 06:40:46 +0100

    modified contract to respect editions and allow transfer of NFT to artists

--- scripts/sellNft.sh
+++ scripts/sellNft.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-goal asset send --amount 1 --assetid 16016195       --from 6OQQDT3FI2FY4TY6XFW7I7QFTSTQWH2TP3AF5U3W42TR6SMQPXEJX7TZAA --to XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --clawback  XA5WEWCKTSR246S7I566J7NLGSBH24QUUAWYOSMBBZOI2DQDFHUB2GO2RU  --out unsignedAssetSend.tx
+goal asset send --amount 1 --assetid 16062688       --from 5CJEIMOPJIKJKSFGIAHNF7NTNUFKZLQQETOWHXEA3AM335B5NQKYHSFVEY --to YI5FXEUEQCFTE7ZAXIM6RSXRE63EAAXVUK2XNYAQVOF4YQXYVSGZNEX4LI --clawback  L5FF6H3PGS6ESQKLM3NM27JS4GYIZMEDG6ZS46WDDIF64MKVIJTZYUETQA  --out unsignedAssetSend.tx
 
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to 6OQQDT3FI2FY4TY6XFW7I7QFTSTQWH2TP3AF5U3W42TR6SMQPXEJX7TZAA --amount 300000 --out unsignedSend.tx
+goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to 5CJEIMOPJIKJKSFGIAHNF7NTNUFKZLQQETOWHXEA3AM335B5NQKYHSFVEY --amount 0 --out unsignedSend.tx
 
-goal app call --from 6OQQDT3FI2FY4TY6XFW7I7QFTSTQWH2TP3AF5U3W42TR6SMQPXEJX7TZAA --app-id 16016196 --out unsignedFreeportCall.tx
+goal app call --from 5CJEIMOPJIKJKSFGIAHNF7NTNUFKZLQQETOWHXEA3AM335B5NQKYHSFVEY --app-id 16062689 --out unsignedFreeportCall.tx
 
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to BN5DMMUOAJCR4SZUGPK3ICQFZ3JXSS24GWDY2B6P56K2SH4EADO6XN56GQ --amount 100000 --out unsignedSend0.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to DG5UXOMPCEPT2SKQ4UJMRNEYDCOABYM3EFR4UW6E3OXF2ME2LF5APS5KLA --amount 100000 --out unsignedSend1.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to DQIEID66TVX55BYECL7UUPIB2OE5ZQGNMFDLE7HRQDNGLTG3UXRBBXOWF4 --amount 100000 --out unsignedSend2.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to EJKLHQS5GRXAKEY2U62YIFBSZPMVTDPL6EQD7XKNN7A33Z7XSYPUCMOODQ --amount 100000 --out unsignedSend3.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to F5N2DN7KIX5EL646GMGV4AQVZHGQJBEIFJR3FEIDVAZP7RGUTBU7LGRRO4 --amount 100000 --out unsignedSend4.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to JYURMSAFWF3BFC6ZJ2WXNQFHBKZONXSVLB6GIH3EH4ZA7Y4PPITDZYVDXU --amount 100000 --out unsignedSend5.tx
-goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to VEKJYW4IIOKGVBI67S5VDQB7ZOHPGJPLRXMI2GG53BAYTB6MZA27S4ALQA --amount 100000 --out unsignedSend6.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to YI5FXEUEQCFTE7ZAXIM6RSXRE63EAAXVUK2XNYAQVOF4YQXYVSGZNEX4LI --amount 100000 --out unsignedSend0.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to IKEPBSW7RSPN4TXYC3AV6FOOGZ6PJLTJKEB2PVCTPSRFNB3CANZ5JJRZPY --amount 100000 --out unsignedSend1.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to 2FSBHE3XAXJHBFFUABPPBBU3ZL4PQAHI6BB3KTHJKL5IZCC7BG4LR6GRT4 --amount 100000 --out unsignedSend2.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to UONII5HLZPHGDCBCETVTFGX42I5MJWEYKM5NUIFQW3A47CSDIHZN74AYUA --amount 100000 --out unsignedSend3.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to EMMEIOWLZPMUCXSLGB5QOR33HCQZAQT6KSRGCXHBO54W7LNLQCJXSGJ4IQ --amount 100000 --out unsignedSend4.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to UNH443RNFL4NWFCP5AI3N34C6IK6SWDEPZRLFKXGWYTXBZ5BTLJJLGWLRQ --amount 100000 --out unsignedSend5.tx
+# goal clerk send --from XHFRWIODL7MFJNCWURZY6USPIWUZTQ2X6EWCJ7SWSRKJPUN4LYHO5XK2BY --to QPNWTRS3FLRUICYYLVPQV7QZIJPBNM2EV6S5BF6JOFQL7DGCLU5HKQSUK4 --amount 100000 --out unsignedSend6.tx
 
 
 
-cat  unsignedAssetSend.tx unsignedSend.tx unsignedFreeportCall.tx  unsignedSend0.tx unsignedSend1.tx unsignedSend2.tx unsignedSend3.tx unsignedSend4.tx unsignedSend5.tx unsignedSend6.tx > combinedNftTransactions.tx
+cat  unsignedAssetSend.tx unsignedSend.tx unsignedFreeportCall.tx  > combinedNftTransactions.tx # unsignedSend0.tx unsignedSend1.tx unsignedSend2.tx unsignedSend3.tx unsignedSend4.tx unsignedSend5.tx unsignedSend6.tx > combinedNftTransactions.tx
 
 
 goal clerk group -i combinedNftTransactions.tx -o groupedNftTransactions.tx 
@@ -29,21 +29,21 @@ goal clerk sign -i splitNft-1.tx -o signoutNft-1.tx
 
 goal clerk sign -i splitNft-2.tx -o signoutNft-2.tx
 
-goal clerk sign -i splitNft-3.tx -o signoutNft-3.tx
+# goal clerk sign -i splitNft-3.tx -o signoutNft-3.tx
 
-goal clerk sign -i splitNft-4.tx -o signoutNft-4.tx
+# goal clerk sign -i splitNft-4.tx -o signoutNft-4.tx
 
-goal clerk sign -i splitNft-5.tx -o signoutNft-5.tx
+# goal clerk sign -i splitNft-5.tx -o signoutNft-5.tx
 
-goal clerk sign -i splitNft-6.tx -o signoutNft-6.tx
+# goal clerk sign -i splitNft-6.tx -o signoutNft-6.tx
 
-goal clerk sign -i splitNft-7.tx -o signoutNft-7.tx
+# goal clerk sign -i splitNft-7.tx -o signoutNft-7.tx
 
-goal clerk sign -i splitNft-8.tx -o signoutNft-8.tx
+# goal clerk sign -i splitNft-8.tx -o signoutNft-8.tx
 
-goal clerk sign -i splitNft-9.tx -o signoutNft-9.tx
+# goal clerk sign -i splitNft-9.tx -o signoutNft-9.tx
 
-cat signoutNft-0.tx signoutNft-1.tx signoutNft-2.tx signoutNft-3.tx signoutNft-4.tx signoutNft-5.tx signoutNft-6.tx signoutNft-7.tx signoutNft-8.tx signoutNft-9.tx > signoutNft.tx
+cat signoutNft-0.tx signoutNft-1.tx signoutNft-2.tx > signoutNft.tx # signoutNft-3.tx signoutNft-4.tx signoutNft-5.tx signoutNft-6.tx signoutNft-7.tx signoutNft-8.tx signoutNft-9.tx > signoutNft.tx
 
 goal clerk rawsend -f signoutNft.tx
 
